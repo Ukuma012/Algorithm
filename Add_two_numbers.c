e67076d7c720fcd16f190624ec7a1c6b5c3e73ae
Ukuma012 <ukuma012@gmail.com>
2023-02-24 10:45:25 +0900

    first commit

---
+++ Add_two_numbers.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+int main(int argc, char *argv[]) {
+    int c;
+    char l1[100];
+    char l2[100];
+    int i = 0;
+    int j = 0;
+    printf("%s", "l1: ");
+    while((c = getchar()) != EOF) {
+        if(c == '\n') {
+            break;
+        }
+        if(c == ' ' || c == '\t') {
+            continue;
+        }
+        l1[i] = c;
+        i++;
+    }
+    printf("%s", "l2: ");
+    while((c = getchar()) != EOF) {
+        if(c == '\n') {
+            break;
+        }
+        if(c == ' ' || c == '\t') {
+            continue;
+        }
+        l2[j] = c;
+        j++;
+    }
+
+
+    return 0;
+}
