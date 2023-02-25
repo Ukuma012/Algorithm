9dd26ccade9b98fedad0ba3f855b5fc061b12e2b
Ukuma012 <ukuma012@gmail.com>
2023-02-25 13:50:07 +0900

    delete

--- Add_two_numbers.c
+++ Add_two_numbers.c
@@ -18,13 +18,10 @@ struct ListNode
     struct ListNode *next;
 };
 
-void insert(struct ListNode **, int);
-void printList(struct ListNode *);
 
 // @TODO まずは一行をlinke list にinsertする
 int main(int argc, char *argv[])
 {
-    struct ListNode *header;
     char *whitespace = " \t";
     char *line = NULL;
     size_t linecap = 0;
@@ -44,39 +41,9 @@ int main(int argc, char *argv[])
             // insert
             // 数値に変換
             int val = *value - '0';
-            insert(&header, val);
+            printf("%d", val);
         }
-        printList(header);
         exit(0);
     }
     return 0;
-}
-
-void insert(struct ListNode **header, int value)
-{
-    struct ListNode *new;
-    if ((new = malloc(sizeof(struct ListNode))) == NULL)
-    {
-        fprintf(stderr, "malloc failed\n");
-        exit(1);
-    }
-    new->val = value;
-    if(*header == NULL) {
-        *header = new;
-    } else {
-        new->next = *header;
-        *header = new;
-    }
-}
-
-void printList(struct ListNode *head)
-{
-    struct ListNode *temp = head;
-
-    while(temp != NULL)
-    {
-         printf("%d->", temp->val);
-         temp = temp->next;
-    }
-    printf("NULL\n");
 }
