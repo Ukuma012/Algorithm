a4beda2cd1fbb89f2ebc19282d3f16142c4e96c8
Ukuma012 <ukuma012@gmail.com>
2023-02-25 13:43:19 +0900

    linked list

--- Add_two_numbers.c
+++ Add_two_numbers.c
@@ -11,20 +11,72 @@
 // add: 342+465 = 807
 // then: 807 -> 7 0 8
 
-struct ListNode {
+
+struct ListNode
+{
     int val;
     struct ListNode *next;
 };
 
+void insert(struct ListNode **, int);
+void printList(struct ListNode *);
+
 // @TODO まずは一行をlinke list にinsertする
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    struct ListNode *header;
+    char *whitespace = " \t";
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
+    char *value;
+
     printf("%s", "l1: ");
-    while((linelen = getline(&line, &linecap, stdin)) > 0) {
-        fwrite(line, linelen, 1, stdout);
+    while ((linelen = getline(&line, &linecap, stdin)) > 0)
+    {
+        while ((value = strsep(&line, whitespace)) != NULL)
+        {
+            if (*value == '\0')
+            {
+                continue;
+            }
+
+            // insert
+            // 数値に変換
+            int val = *value - '0';
+            insert(&header, val);
+        }
+        printList(header);
         exit(0);
     }
     return 0;
+}
+
+void insert(struct ListNode **header, int value)
+{
+    struct ListNode *new;
+    if ((new = malloc(sizeof(struct ListNode))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    new->val = value;
+    if(*header == NULL) {
+        *header = new;
+    } else {
+        new->next = *header;
+        *header = new;
+    }
+}
+
+void printList(struct ListNode *head)
+{
+    struct ListNode *temp = head;
+
+    while(temp != NULL)
+    {
+         printf("%d->", temp->val);
+         temp = temp->next;
+    }
+    printf("NULL\n");
 }
