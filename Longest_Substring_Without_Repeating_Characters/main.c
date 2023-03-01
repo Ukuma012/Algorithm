fe1be3ca1fdd27119515a780cb79ba2adb764c2e
Ukuma012 <ukuma012@gmail.com>
2023-03-01 13:51:30 +0900

    modified

--- Longest_Substring_Without_Repeating_Characters/main.c
+++ Longest_Substring_Without_Repeating_Characters/main.c
@@ -22,13 +22,12 @@ void init() {
 }
 
 int main(int argc, char *argv[]) {
+    init();
+
     char *s = argv[1];
     int i = 0;
-    int j = 0;
     while(s[i] != '\0') {
-        printf("%d %c\n", j, s[i]);
         i++;
-        j++;
     }
     return 0;
 }
