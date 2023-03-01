499757cab7ba7f90466cb08c51f2d4b7eff4deae
Ukuma012 <ukuma012@gmail.com>
2023-03-02 08:47:39 +0900

    argv array

--- 944_Delete_columns_to_make_sorted/main.c
+++ 944_Delete_columns_to_make_sorted/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+    for(int i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
