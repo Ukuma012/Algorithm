0bc26746ee1136ab2f002ed52c795d03a42c5a33
Ukuma012 <ukuma012@gmail.com>
2023-03-02 10:44:55 +0900

    output

--- 944_Delete_columns_to_make_sorted/main.c
+++ 944_Delete_columns_to_make_sorted/main.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 
-int minDeletionSize(char **strs, int strsSize) {
+int minDeletionSize(char **strs, int strsSize)
+{
     int count = 0;
-    for(int i = 1, j = 2; j < strsSize+1; i++, j++) {
+    int flags[1000];
+
+    for (int i = 1, j = 2; j < strsSize + 1; i++, j++)
+    {
         int h = 0;
         char *s1 = strs[i];
         char *s2 = strs[j];
-        while(s1[h] != '\0') {
-            if(s1[h] > s2[h]) {
+        while (s1[h] != '\0')
+        {
+            if (flags[h] == -1)
+            {
+                h++;
+                continue;
+            }
+            if (s1[h] > s2[h])
+            {
+                flags[h] = -1;
                 count++;
                 h++;
                 continue;
@@ -18,7 +30,8 @@ int minDeletionSize(char **strs, int strsSize) {
     return count;
 }
 
-int main(int argc, char **strs) {
+int main(int argc, char **strs)
+{
     int result = 0;
     int strsSize = argc - 1;
 
@@ -26,4 +39,3 @@ int main(int argc, char **strs) {
     printf("%d\n", result);
     return 0;
 }
-
