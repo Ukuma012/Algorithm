882cb1ea63f307ae9061150bf6772cab05e537ae
Ukuma012 <ukuma012@gmail.com>
2023-03-02 10:21:23 +0900

    count

--- 944_Delete_columns_to_make_sorted/main.c
+++ 944_Delete_columns_to_make_sorted/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    for(int i = 1; i < argc; i++) {
-        printf("%s\n", argv[i]);
+int minDeletionSize(char **strs, int strsSize) {
+    int count = 0;
+    for(int i = 1, j = 2; j < strsSize+1; i++, j++) {
+        int h = 0;
+        char *s1 = strs[i];
+        char *s2 = strs[j];
+        while(s1[h] != '\0') {
+            if(s1[h] > s2[h]) {
+                count++;
+                h++;
+                continue;
+            }
+            h++;
+        }
     }
+    return count;
+}
+
+int main(int argc, char **strs) {
+    int result = 0;
+    int strsSize = argc - 1;
+
+    result = minDeletionSize(strs, strsSize);
+    printf("%d\n", result);
     return 0;
 }
+
