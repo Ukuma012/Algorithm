7f4a9f7f0bee23eb1b5dafa6083ecf2e13dd7503
Ukuma012 <ukuma012@gmail.com>
2023-03-03 15:58:12 +0900

    adjacency list

--- 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
+++ 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+// まずはadjacency list of a given graphを作る
+
+// graph
+#define MAXV 20000 // 2 * 10^4
+
+typedef struct {
+    int y;
+    int weight;
+    struct edgenode *next;
+} edgenode;
+
+typedef struct {
+    edgenode *edges[MAXV+1];
+    int degree[MAXV+1];
+    int nvertices;
+    int nedges;
+} graph;
+
 int main(int argc, char *argv[]) {
     printf("%s\n", "Hello World");
     return 0;
