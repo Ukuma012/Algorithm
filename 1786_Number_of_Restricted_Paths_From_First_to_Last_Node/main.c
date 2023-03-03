74b63e084d56ce09064ab2393942e2b7a67e0b74
Ukuma012 <ukuma012@gmail.com>
2023-03-03 16:12:38 +0900

    too many nodes

--- 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
+++ 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
@@ -1,24 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 // 入力を受ける
 // まずはadjacency list of a given graphを作る
 
 // graph
 #define MAXV 20000 // 2 * 10^4
 
-typedef struct {
+typedef struct
+{
     int y;
     int weight;
     struct edgenode *next;
 } edgenode;
 
-typedef struct {
-    edgenode *edges[MAXV+1];
-    int degree[MAXV+1];
+typedef struct
+{
+    edgenode *edges[MAXV + 1];
+    int degree[MAXV + 1];
     int nvertices;
     int nedges;
 } graph;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    int n = atoi(argv[1]);
+    if (n > MAXV)
+    {
+        fprintf(stderr, "too many nodes\n");
+        exit(1);
+    }
+
     printf("%s\n", "Hello World");
+
     return 0;
 }
