5df6f615ba2a7207379ec50b882eb335e82e70cb
Ukuma012 <ukuma012@gmail.com>
2023-03-05 14:23:01 +0900

    graph

--- 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
+++ 1786_Number_of_Restricted_Paths_From_First_to_Last_Node/main.c
@@ -1,42 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // まずはadjacency list of a given graphを作る
-
-// graph
-#define MAXV 20000 // 2 * 10^4
+#define MAXV 5
 
 // @TODO 入力を受け取るように
 int input[21] = {1, 2, 3, 1, 3, 3, 2, 3, 1, 1, 4, 2, 5, 2, 2, 3, 5, 1, 5, 4, 10};
 
-typedef struct
+struct edgenode
 {
     int y;
     int weight;
     struct edgenode *next;
-} edgenode;
+};
 
-typedef struct
+struct graph
 {
-    edgenode *edges[MAXV + 1];
-    int degree[MAXV + 1];
-    int nvertices;
-    int nedges;
-} graph;
+    struct edgenode *edges[MAXV + 1];
+};
+
+void insert_edge(struct graph *g, int x, int y, int weight, bool directed);
+void init_graph(struct graph *g);
+void print_graph(struct graph *g);
 
 int main(int argc, char *argv[])
 {
-    int n = atoi(argv[1]);
-    if (n > MAXV)
+    bool directed = false;
+
+    struct graph *g;
+    if ((g = malloc(sizeof(struct graph))) == NULL)
     {
-        fprintf(stderr, "too many nodes\n");
+        fprintf(stderr, "malloc failed\n");
         exit(1);
     }
 
-    for(int j = 0; j < 21; j++) {
-        printf("%d ", input[j]);
+    init_graph(g);
+
+    for (int j = 0; j < (sizeof(input) / sizeof(int)); j += 3)
+    {
+        insert_edge(g, input[j], input[j + 1], input[j + 2], directed);
     }
 
+    print_graph(g);
 
     return 0;
+}
+
+void print_graph(struct graph *g)
+{
+    int i;
+    struct edgenode *p;
+    if ((p = malloc(sizeof(struct edgenode))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    for (i = 1; i < MAXV + 1; i++)
+    {
+        printf("%d: ", i);
+        p = g->edges[i];
+        while (p != NULL)
+        {
+            printf("%d(%d) ", p->y, p->weight);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
+
+void init_graph(struct graph *g)
+{
+    int i;
+    for (i = 1; i <= MAXV; i++)
+    {
+        g->edges[i] = NULL;
+    }
+}
+
+void insert_edge(struct graph *g, int x, int y, int weight, bool directed)
+{
+    struct edgenode *p;
+    if ((p = malloc(sizeof(struct edgenode))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    };
+
+    p->y = y;
+    p->weight = weight;
+    p->next = g->edges[x];
+
+    g->edges[x] = p;
+
+    if(directed == false) {
+    insert_edge(g, y, x, weight, true);
+    }
 }
