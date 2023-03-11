9c3cff1e441227f644d1f6a7c2cbf0e1e99cb9ed
Ukuma012 <ukuma012@gmail.com>
2023-03-11 21:24:23 +0900

    TreeNdoe

--- 965_Univalued_Binary_Tree/main.c
+++ 965_Univalued_Binary_Tree/main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+struct TreeNode {
+    int value;
+    struct TreeNode *right;
+    struct TreeNode *left;
+};
+
 int main(int argc, char *argv[]) {
 
     printf("%s\n", "Hello World");
