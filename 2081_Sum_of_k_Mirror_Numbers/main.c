ba4120f7ebe4485ee6a18ef2119602b86ff37b1a
Ukuma012 <ukuma012@gmail.com>
2023-03-11 09:23:35 +0900

    base 10

--- 2081_Sum_of_k_Mirror_Numbers/main.c
+++ 2081_Sum_of_k_Mirror_Numbers/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // k = 2, n = 5
 
@@ -7,6 +8,22 @@ int main(int argc, char *argv[]) {
     int k, n;
     k = atoi(argv[1]);
     n = atoi(argv[2]);
+
+    char s[100] = "1222341";
+    int i = 0;
+    int j = strlen(s) -1;
+    while(1) {
+        if(i == j) {
+            break;
+        }
+        if(s[i++] == s[j--]) {
+            printf("%s\n", "yes");
+            continue;
+        } else {
+            printf("%s\n", "No");
+            exit(1);
+        }
+    }
 
     printf("base is %d\n", k);
     printf("%d times\n", n);
