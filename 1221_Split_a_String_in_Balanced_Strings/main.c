1fe15bf0b6ec8ae377db7c99d77c3523b5717c28
Ukuma012 <ukuma012@gmail.com>
2023-03-13 13:03:01 +0900

    count

--- 1221_Split_a_String_in_Balanced_Strings/main.c
+++ 1221_Split_a_String_in_Balanced_Strings/main.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
-    exit(0);
+    int Rcount = 0;
+    int Lcount = 0;
+    int count = 0;
+    int n = 0;
+    char *s = argv[1];
+
+    while(s[n] != '\0') {
+        if(s[n] == 'R') {
+            Rcount++;
+        } else if(s[n] == 'L') {
+            Lcount++;
+        }
+
+        if(Rcount == Lcount) {
+            count++;
+        }
+        n++;
+    }
+    
+    printf("%d\n", count);
+    return 0;
 }
