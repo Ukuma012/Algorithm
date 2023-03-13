271f848753978eb923bcf74c4b9477f9f7ddaf15
Ukuma012 <ukuma012@gmail.com>
2023-03-13 10:59:47 +0900

    heap

--- 965_Univalued_Binary_Tree/main.c
+++ 965_Univalued_Binary_Tree/main.c
@@ -1,18 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // HEAPでやる
 #define MAX_HEAP 100
 int heap[MAX_HEAP + 1];
 int heap_pointer = 1;
 
-int main(int argc, char *argv[]) {
-    for(int i = 1; i < argc; i++) {
-        if(*argv[i] == 'n') {
+int main(int argc, char *argv[])
+{
+
+    for (int i = 1; i < argc; i++)
+    {
+        if (*argv[i] == 'n')
+        {
             heap[heap_pointer++] = -1;
             continue;
         }
-        heap[heap_pointer++] = *argv[i] - '0'; 
+        heap[heap_pointer++] = *argv[i] - '0';
     }
 
+    int root = heap[1];
+    for (int j = 2; j < argc; j++)
+    {
+        if (heap[j] == -1)
+        {
+            continue;
+        }
+
+        if (heap[j] != root)
+        {
+            printf("%s\n", "false");
+            exit(0);
+        }
+    }
+
+    printf("%s\n", "true");
     return 0;
 }
