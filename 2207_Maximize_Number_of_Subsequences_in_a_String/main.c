f50f546428d153360d3a2d224342f369a74f9ce2
Ukuma012 <ukuma012@gmail.com>
2023-03-13 13:42:08 +0900

    find last

--- 2207_Maximize_Number_of_Subsequences_in_a_String/main.c
+++ 2207_Maximize_Number_of_Subsequences_in_a_String/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+// patern[0] -> pattern[1]'s first 
+// patern[1] -> pattern[0]'s last
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+    int n = 0;
+    int pos0 = 0;
+    int pos1 = 0;
+    char *text = argv[1];
+    char *pattern = argv[2];
+
+    while(text[n] != '\0') {
+        if(text[n] == pattern[0]) {
+            pos0 = n;
+        }
+        n++;
+    }
+
+    printf("%d\n", pos0);
+
     return 0;
 }
