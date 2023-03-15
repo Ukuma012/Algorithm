c8c20ac317b8fb894aca164b0429278f601713e6
Ukuma012 <ukuma012@gmail.com>
2023-03-15 11:27:09 +0900

    how many square numbers need

--- 279_Perfect_Squares/main.c
+++ 279_Perfect_Squares/main.c
@@ -8,14 +8,27 @@
 
 int main(int argc, char *argv[]) {
     int j = 0;
+    int n = 0;
     int square[100];
-    for(int i = 1; i <= 100; i++) {
+    int table[TABLE_SIZE];
+
+    for(int i = 1; i < 100; i++) {
         square[j] = i*i;
         j++;
     }
 
+    int m = atoi(argv[1]);
+    for(int i = 0; i < m; i++) {
+        table[i] = 0;
+    }
 
-    // int n = atoi(argv[1]);
-    // printf("%d\n", n);
+    for(int i = 0; i < 100; i++) {
+        if(square[i] <= m) {
+            n++;
+        } else {
+            break;
+        }
+    }
+
     return 0;
 }
