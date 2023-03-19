049695008d28ced0981da5745fd582a97a7a2eb4
Ukuma012 <ukuma012@gmail.com>
2023-03-19 21:55:38 +0900

    stack

--- 841_Keys_and_Rooms/main.c
+++ 841_Keys_and_Rooms/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
 
+// rooms.length
+
+int stack[1000];
+int pointer = 0;
+
 int main(int argc, char *argv[]) {
     // use stack
 
     printf("%s\n", "Hello World");
     return 0;
+}
+
+void push(int x) {
+    stack[pointer++] = x;
+}
+
+int pop() {
+    int x;
+    x = stack[--pointer];
+    return x;
 }
