d72f3b083b060612472b1d2ad300928245e2aefd
Ukuma012 <ukuma012@gmail.com>
2023-03-20 21:28:41 +0900

    input

--- 841_Keys_and_Rooms/main.c
+++ 841_Keys_and_Rooms/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // input: rooms.length
 
@@ -7,6 +8,13 @@ int pointer = 0;
 
 int main(int argc, char *argv[])
 {
+    int c;
+    if((c = getchar()) == EOF) {
+        fprintf(stderr, "getchar failed\n");
+        exit(1);
+    }
+    int roomlength = c - 48;
+    printf("%d\n", roomlength);
 
     return 0;
 }
