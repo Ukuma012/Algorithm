6a0626477de58d9f40f9ae2a957b3a66df0a257c
Ukuma012 <ukuma012@gmail.com>
2023-03-22 08:47:24 +0900

    input

--- 2499_Minimum_Total_Cost_to_Make_Arrays_Unequal/main.c
+++ 2499_Minimum_Total_Cost_to_Make_Arrays_Unequal/main.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#define BUF 256
 
-    if(argc != 2) {
-       fprintf(stderr, "argument!\n");
-       exit(1); 
+int main(int argc, char *argv[])
+{
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "argument!\n");
+        exit(1);
     }
-    printf("%s\n", "Hello World");
+
+    int n = *argv[1] - 48;
+    int nums1[n];
+    int nums2[n];
+    char str1[BUF];
+    char str2[BUF];
+    for (int i = 0; i < n; i++)
+    {
+        int c = 0;
+        if (fgets(str1, BUF, stdin) != NULL)
+        {
+            c = atoi(str1);
+        }
+        nums1[i] = c;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int c = 0;
+        if (fgets(str2, BUF, stdin) != NULL)
+        {
+            c = atoi(str2);
+        }
+        nums2[i] = c;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", nums1[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", nums2[i]);
+    }
     exit(0);
 }
