2fd8bb1a3178054b512be641fb35abea4c759d11
Ukuma012 <ukuma012@gmail.com>
2023-03-22 08:39:46 +0900

    error handling

--- 2499_Minimum_Total_Cost_to_Make_Arrays_Unequal/main.c
+++ 2499_Minimum_Total_Cost_to_Make_Arrays_Unequal/main.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+
+    if(argc != 2) {
+       fprintf(stderr, "argument!\n");
+       exit(1); 
+    }
     printf("%s\n", "Hello World");
     exit(0);
 }
