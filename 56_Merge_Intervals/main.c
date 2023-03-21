8a8430b6edfa1d5343df029ca9092a2ce0c6d42f
Ukuma012 <ukuma012@gmail.com>
2023-03-21 14:06:21 +0900

    intervals

--- 56_Merge_Intervals/main.c
+++ 56_Merge_Intervals/main.c
@@ -1,16 +1,71 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-struct interval {
+struct interval
+{
     int start;
     int end;
+    bool check;
 };
 
 struct interval intervals[10000];
 
-int main(int argc, char *argv[]) {
+// sorting on start
+
+int main(int argc, char *argv[])
+{
+    int count = 0;
+    int previous;
+    int current;
     intervals[0].start = 1;
     intervals[0].end = 3;
+    intervals[0].check = true;
+    intervals[1].start = 2;
+    intervals[1].end = 6;
+    intervals[1].check = true;
+    intervals[2].start = 8;
+    intervals[2].end = 10;
+    intervals[2].check = true;
+    intervals[3].start = 15;
+    intervals[3].end = 18;
+    intervals[3].check = true;
 
-    printf("%d\n", intervals[0].start);
+    while (count != (3 - 1))
+    {
+        previous = 0;
+        current = 1;
+        if (intervals[previous].end < intervals[current].start)
+        {
+            previous++;
+            current++;
+            count++;
+            continue;
+        }
+        else if (intervals[previous].end >= intervals[current].start && intervals[previous].end <= intervals[current].end) {
+            intervals[previous].end = intervals[current].end;
+            intervals[current].check = false;
+            previous++;
+            current++;
+            count++;
+            continue;
+        } else if(intervals[previous].end >= intervals[current].end) {
+            intervals[current].check = false;
+            previous++;
+            current++;
+            count++;
+            continue;
+        }
+    }
+
+    for(int i = 0; i < 3; i++) {
+        if(intervals[i].check == false) {
+            continue;
+        }
+
+        printf("[%d %d]", intervals[i].start, intervals[i].end);
+    }
+
+    printf("\n");
+
     return 0;
 }
