d8f9e3679eea92259d99ea1195b73504b9e3ada0
Ukuma012 <ukuma012@gmail.com>
2023-03-21 15:57:27 +0900

    input

--- 2596_Check_Knight_Tour_Configuration/main.c
+++ 2596_Check_Knight_Tour_Configuration/main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
-    exit(1);
+#define BUF 256
+
+// ./main (number of col)
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "number of col need\n");
+        exit(1);
+    }
+    int n = *argv[1] - 48;
+    int grid[n][n];
+    char str[BUF];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+                int c = 0;
+                if(fgets(str, BUF, stdin) != NULL) {
+                    c = atoi(str);
+                }
+                grid[i][j] = c;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            printf("%2d ", grid[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
