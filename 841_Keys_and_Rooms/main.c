d316d5c7357694b91fe98e385f756b13e7fb52ac
Ukuma012 <ukuma012@gmail.com>
2023-03-21 10:27:58 +0900

    check

--- 841_Keys_and_Rooms/main.c
+++ 841_Keys_and_Rooms/main.c
@@ -5,9 +5,15 @@
 
 int stack[1000];
 int pointer = 0;
+int counter = 0;
+
+void check();
+void push(int );
+int pop();
 
 int main(int argc, char *argv[])
 {
+    int room = 0;
     int c;
     if((c = getchar()) == EOF) {
         fprintf(stderr, "getchar failed\n");
@@ -15,8 +21,21 @@ int main(int argc, char *argv[])
     }
     int roomlength = c - 48;
     printf("%d\n", roomlength);
+    push(1);
+    check();
 
     return 0;
+}
+
+void check() {
+    while(pointer != 0) {
+        int n = pop();
+        if(n == ++counter) {
+            printf("%s\n", "Success from check\n");
+            exit(0);
+        }
+    }
+    printf("%s\n", "Failed\n");
 }
 
 void push(int x)
