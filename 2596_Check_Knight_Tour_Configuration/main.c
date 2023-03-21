6063009905ae06a1cf05181e03d7a9cf33e76f77
Ukuma012 <ukuma012@gmail.com>
2023-03-21 21:19:11 +0900

    OK

--- 2596_Check_Knight_Tour_Configuration/main.c
+++ 2596_Check_Knight_Tour_Configuration/main.c
@@ -43,48 +43,67 @@ int main(int argc, char *argv[])
 
     while (current != (n * n - 1))
     {
-        if (grid[row += 1][col += 2] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row += 2][col += 1] == (current + 1))
-        {
-            printf("%d %d\n", row, col);
-            current++;
-            continue;
-        }
-        else if (grid[row -= 1][col += 2] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row -= 2][col += 1] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row += 2][col -= 1] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row += 1][col -= 2] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row -= 1][col -= 2] == (current + 1))
-        {
-            current++;
-            continue;
-        }
-        else if (grid[row -= 2][col -= 1] == (current + 1))
-        {
-            current++;
-            continue;
-        }
+        int row1, row2, row3, row4, row5, row6, row7, row8;
+        int col1, col2, col3, col4, col5, col6, col7, col8;
+        row1 = row2 = row3 = row4 = row5 = row6 = row7 = row8 = row;
+        col1 = col2 = col3 = col4 = col5 = col6 = col7 = col8 = col;
 
+        if (grid[row1 += 1][col1 += 2] == (current + 1))
+        {
+            current++;
+            row = row1;
+            col = col1;
+            continue;
+        }
+        else if (grid[row2 += 2][col2 += 1] == (current + 1))
+        {
+            current++;
+            row = row2;
+            col = col2;
+            continue;
+        }
+        else if (grid[row3 -= 1][col3 += 2] == (current + 1))
+        {
+            current++;
+            row = row3;
+            col = col3;
+            continue;
+        }
+        else if (grid[row4 -= 2][col4 += 1] == (current + 1))
+        {
+            current++;
+            row = row4;
+            col = col4;
+            continue;
+        }
+        else if (grid[row5 += 2][col5 -= 1] == (current + 1))
+        {
+            current++;
+            row = row5;
+            col = col5;
+            continue;
+        }
+        else if (grid[row6 += 1][col6 -= 2] == (current + 1))
+        {
+            current++;
+            row = row6;
+            col = col6;
+            continue;
+        }
+        else if (grid[row7 -= 1][col7 -= 2] == (current + 1))
+        {
+            current++;
+            row = row7;
+            col = col7;
+            continue;
+        }
+        else if (grid[row8 -= 2][col8 -= 1] == (current + 1))
+        {
+            current++;
+            row = row8;
+            col = col8;
+            continue;
+        }
         printf("%s\n", "Failed!");
         exit(0);
     }
