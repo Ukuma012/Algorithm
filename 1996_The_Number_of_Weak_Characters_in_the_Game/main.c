4f9e999a50558b92e9459a768a3b63ed150c4873
YuKumagai <ukumaubiz@gmail.com>
2023-03-23 08:28:25 +0900

    input

--- 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
+++ 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
@@ -3,6 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello World");
+    if (argc != 2)
+    {
+        fprintf(stderr, "argument!\n");
+        exit(1);
+    }
+
+    int n = *argv[1] - 48;
+    int properties[2 * n];
+    char str[2046];
+
+    for (int i = 0; i < (2 * n); i++)
+    {
+        int c = 0;
+        if (fgets(str, 2046, stdin) != NULL)
+        {
+            c = atoi(str);
+        }
+        properties[i] = c;
+    }
+
+    for (int i = 0; i < (2 * n); i++)
+    {
+        printf("%d ", properties[i]);
+    }
+    printf("\n");
     exit(0);
 }
