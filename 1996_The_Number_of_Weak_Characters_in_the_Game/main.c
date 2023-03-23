784702fc927df317f9d0a90ae915cc70ab023336
Ukuma012 <ukuma012@gmail.com>
2023-03-23 17:31:23 +0900

    attack defence input

--- 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
+++ 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
@@ -3,7 +3,7 @@
 
 struct property {
     int attack;
-    int defence;
+    int defense;
 };
 
 // stack
@@ -17,22 +17,29 @@ int main(int argc, char *argv[])
     }
 
     int n = *argv[1] - 48;
-    int properties[n];
-    char str[2046];
+    struct property properties[n];
+    char str_attack[2046];
+    char str_defense[2046];
 
-    for (int i = 0; i < (2 * n); i++)
+    for (int i = 0; i < n; i++)
     {
-        int c = 0;
-        if (fgets(str, 2046, stdin) != NULL)
-        {
-            c = atoi(str);
+            int c = 0;
+            if(fgets(str_attack, 2046, stdin) != NULL) {
+                c = atoi(str_attack);
+            }
+            properties[i].attack = c;
+
+            int d = 0;
+            if(fgets(str_defense, 2046, stdin) != NULL) {
+                d = atoi(str_defense);
+            }
+            properties[i].defense = d;
         }
-        properties[i] = c;
-    }
 
-    for (int i = 0; i < (2 * n); i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("%d ", properties[i]);
+        printf("%d ", properties[i].attack);
+        printf("%d ", properties[i].defense);
     }
     printf("\n");
     exit(0);
