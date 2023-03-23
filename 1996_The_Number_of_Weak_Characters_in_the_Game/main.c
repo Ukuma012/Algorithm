21b7d5c83717fc762ddba182e880f07344e21b4f
Ukuma012 <ukuma012@gmail.com>
2023-03-24 00:12:42 +0900

    delete comments

--- 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
+++ 1996_The_Number_of_Weak_Characters_in_the_Game/main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {
+typedef struct
+{
     int attack;
     int defense;
 } property;
 
-// qsort
-// stack
-
-int compar(const void* a, const void* b) {
-    if (((property*)a)->attack >= ((property*)b)->attack) {
+int compar(const void *a, const void *b)
+{
+    if (((property *)a)->attack >= ((property *)b)->attack)
+    {
         return 1;
-    } else {
+    }
+    else
+    {
         return -1;
     }
 }
@@ -32,18 +34,20 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < n; i++)
     {
-            int c = 0;
-            if(fgets(str_attack, 2046, stdin) != NULL) {
-                c = atoi(str_attack);
-            }
-            properties[i].attack = c;
-
-            int d = 0;
-            if(fgets(str_defense, 2046, stdin) != NULL) {
-                d = atoi(str_defense);
-            }
-            properties[i].defense = d;
+        int c = 0;
+        if (fgets(str_attack, 2046, stdin) != NULL)
+        {
+            c = atoi(str_attack);
         }
+        properties[i].attack = c;
+
+        int d = 0;
+        if (fgets(str_defense, 2046, stdin) != NULL)
+        {
+            d = atoi(str_defense);
+        }
+        properties[i].defense = d;
+    }
 
     qsort(properties, n, sizeof(property), compar);
 
