e63359e1c7aea83a21188fd4d4499d4eb40ee280
Ukuma012 <ukuma012@gmail.com>
2023-03-24 10:28:17 +0900

    binary tree

--- 437_Path_Sum_III/main.c
+++ 437_Path_Sum_III/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+
+    if(argc != 2) {
+        fprintf(stderr, "argument!");
+        exit(0);
+    }
+
+    int targetSum = *argv[1] - 48;
+
+    printf("%d\n", targetSum);
     exit(0);
 }
