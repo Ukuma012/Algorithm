96fa5d0c0a3f5b801fb7011ab411c288006ea571
Ukuma012 <ukuma012@gmail.com>
2023-03-25 21:47:26 +0900

    input

--- 2096_Step_By_Step_Directions_From_a_Binary_Tree_Node_to_Another/main.c
+++ 2096_Step_By_Step_Directions_From_a_Binary_Tree_Node_to_Another/main.c
@@ -1,7 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct TreeNode {
+    int val;
+    struct TreeNode *right;
+    struct TreeNode *left;
+};
+
+struct TreeNode* createTree(int val) {
+    struct TreeNode *new;
+    if((new = malloc(sizeof(struct TreeNode))) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    new->left = NULL;
+    new->right = NULL;
+    new->val = val;
+
+    return new;
+}
+
+void insertRight(struct TreeNode *node, int val) {
+    struct TreeNode *p;
+    if(((p = malloc(sizeof(struct TreeNode))) == NULL)) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    p->left = NULL;
+    p->right = NULL;
+    p->val = val;
+    node->right = p;
+}
+
+void insertLeft(struct TreeNode *node, int val) {
+    struct TreeNode *p;
+    if(((p = malloc(sizeof(struct TreeNode))) == NULL)) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    p->left = NULL;
+    p->right = NULL;
+    p->val = val;
+    node->left = p;
+}
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+    // root = [5,1,2,3,null,6,4]
+    struct TreeNode *root = createTree(5);
+    insertLeft(root, 1);
+    insertRight(root, 2);
+    insertLeft(root->left, 3);
+
+    insertLeft(root->right, 6);
+    insertRight(root->right, 4);
+
+    printf("%d\n", root->val);
+    printf("%d\n", root->right->right->val);
     exit(0);
 }
