44f79787f2076fab3c2e6edbc9cfcf088e3da5cd
Ukuma012 <ukuma012@gmail.com>
2023-03-25 11:19:41 +0900

    input modified

--- 437_Path_Sum_III/main.c
+++ 437_Path_Sum_III/main.c
@@ -77,6 +77,8 @@ void count(struct TreeNode* node, int targetSum) {
         count(node->left, targetSum);
         count(node->right, targetSum);
     }
+
+    return;
 }
 
 int main(int argc, char *argv[])
@@ -88,19 +90,35 @@ int main(int argc, char *argv[])
         exit(0);
     }
 
-    int targetSum = *argv[1] - 48;
+    int targetSum = atoi(argv[1]);
     // root = [10, 5, -3, 3, 2, null, 11, 3, -2, null, 1]
-    struct TreeNode *root = createNode(10);
-    insertleft(root, 5);
-    insertright(root, -3);
-    insertleft(root->left, 3);
-    insertright(root->left, 2);
+    // struct TreeNode *root = createNode(10);
+    // insertleft(root, 5);
+    // insertright(root, -3);
+    // insertleft(root->left, 3);
+    // insertright(root->left, 2);
 
-    insertright(root->right, 11);
-    insertleft(root->left->left, 3);
-    insertright(root->left->left, -2);
+    // insertright(root->right, 11);
+    // insertleft(root->left->left, 3);
+    // insertright(root->left->left, -2);
 
-    insertright(root->left->right, 1);
+    // insertright(root->left->right, 1);
+
+    // root = [5,4,8,11,null,13,4,7,2,null,null,5,1]
+    struct TreeNode *root = createNode(5);
+    insertleft(root, 4);
+    insertright(root, 8);
+
+    insertleft(root->left, 11);
+
+    insertleft(root->right, 13);
+    insertright(root->right, 4);
+
+    insertleft(root->left->left, 7);
+    insertright(root->left->left, 2);
+
+    insertleft(root->right->right, 5);
+    insertright(root->right->right, 1);
 
     count(root, targetSum);
     printf("%d\n", answer);
