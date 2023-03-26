2cebf614e85e08c1b1b3a430ee39d3482f48d20b
Ukuma012 <ukuma012@gmail.com>
2023-03-26 13:24:30 +0900

    modified

--- 916_Word_Subsets/main.c
+++ 916_Word_Subsets/main.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // O(n) solution!!
 
-int bucket[26];
+struct bucket {
+    int val;
+    bool set;
+};
 
-int hash(char c) {
+struct bucket buckets[26];
+
+int hash(char c)
+{
     int n = c - 97;
     return n;
 }
 
-int main(int argc, char *argv[]) {
+void bucket_init()
+{
+    for (int i = 0; i < 26; i++)
+    {
+        buckets[i].set = false;
+        buckets[i].val = 0;
+    }
+}
 
-    int n = hash('a');
-    printf("%d\n", n);
+int main(int argc, char *argv[])
+{
+    bucket_init();
+    char *words1[5] = {"amazon", "apple", "facebook", "google", "leetcode"};
+    char *words2[2] = {"e", "o"};
+
+    for(int i = 0; i < 2; i++) {
+        char *c = words2[i];
+        for(int j = 0; j < 10; j++) {
+            if(c[j] == '\0') {
+                break;
+            }
+            int n = hash(c[j]);
+            buckets[n].val++; 
+            if(buckets[n].set == false) {
+                buckets[n].set = true;
+            }
+        }
+    }
+
+    for(int i = 0; i < 26; i++) {
+        printf("%d ", buckets[i].val);
+    }
     exit(0);
 }
