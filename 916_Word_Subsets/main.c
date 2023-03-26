1a07b9b89a5b3f3cda469759bdaa98c11e53af30
Ukuma012 <ukuma012@gmail.com>
2023-03-26 11:43:40 +0900

    hash

--- 916_Word_Subsets/main.c
+++ 916_Word_Subsets/main.c
@@ -3,7 +3,16 @@
 
 // O(n) solution!!
 
+int bucket[26];
+
+int hash(char c) {
+    int n = c - 97;
+    return n;
+}
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+
+    int n = hash('a');
+    printf("%d\n", n);
     exit(0);
 }
