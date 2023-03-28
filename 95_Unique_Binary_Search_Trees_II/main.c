5cafe8c69e11261f1f76c9e5c227965b962b9ca1
Ukuma012 <ukuma012@gmail.com>
2023-03-28 21:28:00 +0900

    tree node

--- 95_Unique_Binary_Search_Trees_II/main.c
+++ 95_Unique_Binary_Search_Trees_II/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
 
