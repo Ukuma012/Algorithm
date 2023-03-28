96c26695ae221ccc981515c9879c8ff6cd451693
Ukuma012 <ukuma012@gmail.com>
2023-03-28 21:37:49 +0900

    generate Node

--- 95_Unique_Binary_Search_Trees_II/main.c
+++ 95_Unique_Binary_Search_Trees_II/main.c
@@ -7,8 +7,24 @@ struct TreeNode {
     struct TreeNode *right;
 };
 
+struct TreeNode* generateNode(int val) {
+    struct TreeNode *new;
+    if((new = malloc(sizeof(struct TreeNode))) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    new->val = val;
+    new->left = NULL;
+    new->right = NULL;
+
+    return new;
+}
+
 int main(int argc, char *argv[]) {
     int n = atoi(argv[1]);
+
+
 
     printf("%d\n", n);
     exit(0);
