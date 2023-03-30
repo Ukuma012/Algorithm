c76a1cd801801d5d00e22c5a35915b4c38477011
Ukuma012 <ukuma012@gmail.com>
2023-03-30 10:04:00 +0900

    unique

--- 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/main.c
+++ 1239_Maximum_Length_of_a_Concatenated_String_with_Unique_Characters/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define arr_size 3
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+    char *arr[arr_size] = {"un", "iq", "ue"};
+
+    for(int i = 0; i < arr_size; i++) {
+        char *s = arr[i];
+        for(int j = 0; j < 26; j++) {
+            if(s[j] == '\0') {
+                break;
+            }
+            printf("%c\n", s[j]);
+        }
+    }
+
     exit(0);
 }
