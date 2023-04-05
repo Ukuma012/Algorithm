79ed80f30238526711fd3e14d3b74f862dcc91e1
Ukuma012 <ukuma012@gmail.com>
2023-04-05 13:45:57 +0900

    graph

--- 1377_Frog_Position_After_T_Seconds/main.c
+++ 1377_Frog_Position_After_T_Seconds/main.c
@@ -1,7 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define edgesSize 7
+
+int input[edgesSize * 2] = {1, 2, 1, 3, 1, 7, 2, 4, 2, 6, 3, 5};
+
+struct edgenode {
+    int val;
+    struct edgenode *next;
+};
+
+struct graph {
+    struct edgenode *edges[edgesSize+1];
+};
+
+void insert(struct graph *g, int x, int val, bool directed) {
+    struct edgenode *new;
+    if((new = malloc(sizeof(struct edgenode))) == NULL) {
+        fprintf(stderr, "malloc faild\n");
+        exit(1);
+    }
+
+    new->val = val;
+    new->next = g->edges[x];
+    g->edges[x] = new;
+
+    if(directed == false) {
+        insert(g, val, x, true);
+    }
+}
+
+void print_graph(struct graph *g)
+{
+    int i;
+    struct edgenode *p;
+    if ((p = malloc(sizeof(struct edgenode))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    for (i = 1; i < edgesSize+1; i++)
+    {
+        printf("%d: ", i);
+        p = g->edges[i];
+        while (p != NULL)
+        {
+            printf("%d ", p->val);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+
+    struct graph *g;
+    if((g = malloc(sizeof(struct graph))) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < (sizeof(input) / sizeof(int)); i += 2) {
+        insert(g, input[i], input[i+1], false);
+    }
+
+    print_graph(g);
+
     exit(0);
 }
