2dcce2d40ac20216ccfbe76b754836cb9eabaa85
Ukuma012 <ukuma012@gmail.com>
2023-04-07 10:06:16 +0900

    row col

--- 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums/main.c
+++ 1605_Find_Valid_Matrix_Given_Row_and_Column_Sums/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define rowSumsize 2
+#define colSumsize 2
+
+int rowSum[rowSumsize] = {3, 8};
+int colSum[colSumsize] = {4, 7};
+
+
 int main(int argc, char *argv[]) {
     printf("%s\n", "Hello World");
     exit(0);
