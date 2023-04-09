fcdd1e76ab7e166179607bf1472c4b52d9a34e87
Ukuma012 <ukuma012@gmail.com>
2023-04-09 20:02:30 +0900

    insert

--- 2497_Maximum_Star_Sum_of_a_Graph/main.c
+++ 2497_Maximum_Star_Sum_of_a_Graph/main.c
@@ -1,18 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define valsize 7
 
-struct TreeNode {
-    int number;
+int vals[valsize] = {1,2,3,4,10,-10,-20};
+int edges[12] = {0, 1, 1, 2, 1, 3, 3, 4, 3, 5, 3, 6};
+
+struct node {
     int val;
+    struct node *next;
 };
 
 struct graph {
-    struct TreeNode *nodes[valsize];
+    struct node *nodes[valsize];
 };
 
+void insert_edge(struct graph *g, int number, int edge, bool directed)
+{
+    struct node *p;
+    if ((p = malloc(sizeof(struct node))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    };
+
+    p->val = vals[edge];
+
+    p->next = g->nodes[number];
+    g->nodes[number] = p;
+
+    if (directed == false)
+    {
+        insert_edge(g, edge, number, true);
+    }
+}
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+
+    bool directed = false;
+
+    struct graph *g;
+    if ((g = malloc(sizeof(struct graph))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < (sizeof(edges) / sizeof(int)); i+=2) {
+        insert_edge(g, edges[i], edges[i+1], directed);
+    }
+
+
     exit(0);
 }
