82fcfea2c9c2938e8f759235ca2777243ab39c66
Ukuma012 <ukuma012@gmail.com>
2023-04-09 10:44:32 +0900

    tree

--- 2497_Maximum_Star_Sum_of_a_Graph/main.c
+++ 2497_Maximum_Star_Sum_of_a_Graph/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct TreeNode {
+    int number;
+    int val;
+};
+
 int main(int argc, char *argv[]) {
     printf("%s\n", "Hello World");
     exit(0);
