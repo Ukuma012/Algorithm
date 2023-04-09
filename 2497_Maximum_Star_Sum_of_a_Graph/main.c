0f9221c3b82a0e66213c94367b32a1425ad29c42
Ukuma012 <ukuma012@gmail.com>
2023-04-09 10:54:04 +0900

    graph

--- 2497_Maximum_Star_Sum_of_a_Graph/main.c
+++ 2497_Maximum_Star_Sum_of_a_Graph/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define valsize 7
+
 struct TreeNode {
     int number;
     int val;
+};
+
+struct graph {
+    struct TreeNode *nodes[valsize];
 };
 
 int main(int argc, char *argv[]) {
