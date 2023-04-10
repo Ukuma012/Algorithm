526ab066351bd5993b5d9119e72937f2ffb33897
YuKumagai <ukumaubiz@gmail.com>
2023-04-10 16:40:46 +0900

    node

--- 1334_Find_the_city_with_the_Smallest_Number_of_neighbors_at_a_Threshold_Distance/main.c
+++ 1334_Find_the_city_with_the_Smallest_Number_of_neighbors_at_a_Threshold_Distance/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+#define nodesize 100
+
+struct node
+{
+    int val;
+    int weight;
+    struct node *next;
+};
+
+struct graph
+{
+    struct node *nodes[nodesize];
+};
+
+int main(int argc, char *argv[])
+{
     printf("%s\n", "Hello World");
     exit(0);
 }
