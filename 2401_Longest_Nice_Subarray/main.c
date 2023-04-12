4decf90496964cfa72d415f00a6d4314fd0f907a
Ukuma012 <Ukuma012@gmail.com>
2023-04-12 14:42:22 +0900

    operator

--- 2401_Longest_Nice_Subarray/main.c
+++ 2401_Longest_Nice_Subarray/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define numsSize 5
+int nums[numsSize] = {1, 3, 8, 48, 10};
+
 int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello World");
+    int max = 0;
+    for (int i = 0; i < numsSize; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < numsSize; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            if ((nums[i] & nums[j]) == 0)
+            {
+                printf("%d %d\n", nums[i], nums[j]);
+                count++;
+                if (max < count)
+                {
+                    max = count;
+                }
+            }
+        }
+    }
+    printf("%d\n", max);
     exit(0);
 }
