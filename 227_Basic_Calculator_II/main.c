48ec285dfd1d654bb59001dc8f83ac46712d958a
Ukuma012 <Ukuma012@gmail.com>
2023-04-12 14:59:15 +0900

    add a comment

--- 227_Basic_Calculator_II/main.c
+++ 227_Basic_Calculator_II/main.c
@@ -13,9 +13,14 @@ int main(int argc, char *argv[])
     char *s = argv[1];
     for (int i = 0; i < maxlength; i++)
     {
+        // + - * / と space
         if (s[i] == '\0')
         {
             break;
+        }
+        if (s[i] == '+')
+        {
+            printf("%s\n", "plus");
         }
         printf("%c\n", s[i]);
     }
