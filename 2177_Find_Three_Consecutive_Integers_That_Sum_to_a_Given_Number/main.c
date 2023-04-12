faf4adb7eaf993bfa290fb9960777b4284a577f3
Ukuma012 <Ukuma012@gmail.com>
2023-04-12 13:51:13 +0900

    maybe

--- 2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number/main.c
+++ 2177_Find_Three_Consecutive_Integers_That_Sum_to_a_Given_Number/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+int main(int argc, char *argv[])
+{
+    int target = 3789;
+
+    for (int i = 0; i < 10000; i++)
+    {
+        if ((i + (i + 1) + (i + 2)) == target)
+        {
+            printf("%d %d %d\n", i, i + 1, i + 2);
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
     exit(0);
 }
