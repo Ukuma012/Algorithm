21a87bb84b1f5fcfb8eef3d7ee52aa71ecb5ebbf
Ukuma012 <ukuma012@gmail.com>
2023-04-15 21:24:32 +0900

    mmm

--- 648_Replace_Words/main.c
+++ 648_Replace_Words/main.c
@@ -7,15 +7,36 @@
 char *dictionary[dictionarySize] = {"cat", "bat", "rat"};
 char sentence[] = "the cattle was rattled by the battery";
 
-int main(int argc, char *argv[]) {
+char *check(char *token, int tokensize)
+{
+    for (int i = 1; i < tokensize; i++)
+    {
+        char prefix[i + 1];
+        strlcpy(prefix, token, sizeof(prefix));
+        for (int j = 0; j < dictionarySize; j++)
+        {
+            if (strcmp(prefix, dictionary[j]) == 0)
+            {
+                return dictionary[j];
+            }
+        }
+    }
+    return token;
+}
+
+int main(int argc, char *argv[])
+{
     char *delim = " ";
     char *token;
 
     token = strtok(sentence, delim);
 
-    while(token != NULL) {
-        // ここで処理
-        printf("%s\n", token);
+    while (token != NULL)
+    {
+        int tokensize = strlen(token);
+        char *word;
+        word = check(token, tokensize);
+        printf("%s\n", word);
         token = strtok(NULL, delim);
     }
 
