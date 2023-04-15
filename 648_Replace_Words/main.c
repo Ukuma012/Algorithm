59d151a6169008b544fd04d43840201e8b23dda8
Ukuma012 <ukuma012@gmail.com>
2023-04-15 15:50:11 +0900

    strtok

--- 648_Replace_Words/main.c
+++ 648_Replace_Words/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define dictionarySize 3
+
+char *dictionary[dictionarySize] = {"cat", "bat", "rat"};
+char sentence[] = "the cattle was rattled by the battery";
 
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello");
+    char *delim = " ";
+    char *token;
+
+    token = strtok(sentence, delim);
+
+    while(token != NULL) {
+        printf("%s\n", token);
+        token = strtok(NULL, delim);
+    }
+
     exit(0);
 }
