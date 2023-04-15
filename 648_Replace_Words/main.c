64305f27c94d538d05aeef94409ca57c4f214d1c
Ukuma012 <ukuma012@gmail.com>
2023-04-15 14:07:56 +0900

    space

--- 648_Replace_Words/main.c
+++ 648_Replace_Words/main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define dictionarySize 3
+#define sentencelength 1000000
+
+char *dictionary[dictionarySize] = {"cat", "bat", "rat"};
+char *sentence = "the cattle was rattled by the battery";
+
 int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello World");
+    for(int i = 0; i < sentencelength; i++) {
+        if(sentence[i] == '\0') {
+            break;
+        }
+        if(sentence[i] == ' ') {
+            printf("%s\n", "space");
+        }
+    }
     exit(0);
 }
