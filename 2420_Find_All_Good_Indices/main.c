bfbc04b674e9fa62c169dda8ff571201cab11295
Ukuma012 <ukuma012@gmail.com>
2023-04-15 21:45:54 +0900

    input

--- 2420_Find_All_Good_Indices/main.c
+++ 2420_Find_All_Good_Indices/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define numsSize 7
+
+int k = 2;
+int nums[numsSize] = {2,1,1,1,3,4,1};
+
 int main(int argc, char *argv[]) {
     printf("%s\n", "Hello");
     exit(0);
