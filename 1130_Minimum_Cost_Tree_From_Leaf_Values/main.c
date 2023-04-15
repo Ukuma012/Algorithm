9047775debd6bc50992e267212c721d9b43bfdbf
Ukuma012 <ukuma012@gmail.com>
2023-04-15 13:40:34 +0900

    min

--- 1130_Minimum_Cost_Tree_From_Leaf_Values/main.c
+++ 1130_Minimum_Cost_Tree_From_Leaf_Values/main.c
@@ -1,8 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define arrsize 3
+
+int arr[arrsize] = {6, 2, 4};
+
 int main(int argc, char *argv[])
 {
+    int n = arrsize;
+    int sum = 0;
+    while (n > 1)
+    {
+        int min_product = INT16_MAX;
+        int min_elm = -1;
+        for (int i = 0; i < arrsize - 1; i++)
+        {
+            int x = arr[i] * arr[i + 1];
+            if (x < min_product)
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    min_elm = i;
+                }
+                else
+                {
+                    min_elm = i + 1;
+                }
+                min_product = x;
+            }
+        }
+        sum += min_product;
+    }
+
     printf("%s\n", "Hello World");
     exit(0);
 }
