83814d21fe582784002643aa25c24b969f7f7327
Ukuma012 <ukuma012@gmail.com>
2023-04-16 08:55:34 +0900

    print

--- 1976_Number_of_Ways_to_Arrive_at_Destination/main.c
+++ 1976_Number_of_Ways_to_Arrive_at_Destination/main.c
@@ -1,7 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#define nodesize 7 
+#define roadssize 30
+
+int roads[roadssize] = {0, 6, 7, 0, 1, 2, 1, 2, 3, 1, 3, 3, 6, 3, 3, 3, 5, 1, 6, 5, 1, 2, 5, 1, 0, 4, 5, 4, 6, 2};
+
+struct node {
+    int val;
+    int weight;
+    struct node *next;
+};
+
+struct graph {
+    struct node *nodes[nodesize];
+};
+
+void insert_edge(struct graph *g, int from_edge, int to_edge, int weight, bool directed)
+{
+    struct node *new;
+
+    if ((new = malloc(sizeof(struct node))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    };
+
+    new->val = to_edge;
+    new->weight = weight;
+
+    new->next = g->nodes[from_edge];
+    g->nodes[from_edge] = new;
+
+    if (directed == false)
+    {
+        insert_edge(g, to_edge, from_edge, weight, true);
+    }
+}
+
+void print_graph(struct graph *g)
+{
+    struct node *p;
+    if ((p = malloc(sizeof(struct node))) == NULL)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    for (int i = 0; i < nodesize; i++)
+    {
+        p = g->nodes[i];
+        printf("%d: ", i);
+        while (p != NULL)
+        {
+            printf("%d(%d) ", p->val, p->weight);
+            p = p->next;
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char *argv[]) {
-    printf("%s\n", "hello");
+    struct graph *g;
+    if((g = malloc(sizeof(struct graph))) == NULL) {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < roadssize; i += 3) {
+        insert_edge(g, roads[i], roads[i+1], roads[i+2], false);
+    }
+
+    print_graph(g);
+
     exit(0);
 }
