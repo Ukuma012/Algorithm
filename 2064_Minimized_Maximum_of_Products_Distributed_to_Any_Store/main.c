246b36b3b2067fc9b899e7cb99b201350fff6023
Ukuma012 <ukuma012@gmail.com>
2023-04-17 22:07:51 +0900

    input

--- 2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store/main.c
+++ 2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define quantitiesSize 2
+
+int n = 6;
+int quantities[quantitiesSize] = {11, 6};
+
 int main(int argc, char *argv[])
 {
     printf("%s\n", "Hello");
