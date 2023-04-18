a981f0343999a2bbe6addff764423516684f1335
Ukuma012 <Ukuma012@gmail.com>
2023-04-18 15:05:51 +0900

    create tree

--- 1145_Binary_Tree_Coloring_Game/main.c
+++ 1145_Binary_Tree_Coloring_Game/main.c
@@ -27,6 +27,32 @@ struct TreeNode *createNode(int val)
 
 int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello");
+    struct TreeNode *root = createNode(1);
+    struct TreeNode *node2 = createNode(2);
+    struct TreeNode *node3 = createNode(3);
+    struct TreeNode *node4 = createNode(4);
+    struct TreeNode *node5 = createNode(5);
+    struct TreeNode *node6 = createNode(6);
+    struct TreeNode *node7 = createNode(7);
+    struct TreeNode *node8 = createNode(8);
+    struct TreeNode *node9 = createNode(9);
+    struct TreeNode *node10 = createNode(10);
+    struct TreeNode *node11 = createNode(11);
+
+    root->left = node2;
+    root->right = node3;
+
+    node2->left = node4;
+    node2->right = node5;
+
+    node3->left = node6;
+    node3->right = node7;
+
+    node4->left = node8;
+    node4->right = node9;
+
+    node5->left = node10;
+    node5->right = node11;
+
     exit(0);
 }
