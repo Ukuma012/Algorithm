1610798b175f2ead631d8213972c7523311e5b89
Ukuma012 <Ukuma012@gmail.com>
2023-04-18 14:44:00 +0900

    tree

--- 1145_Binary_Tree_Coloring_Game/main.c
+++ 1145_Binary_Tree_Coloring_Game/main.c
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+struct TreeNode
+{
+    int val;
+    struct TreeNode *right;
+    struct TreeNode *left;
+};
+
 int main(int argc, char *argv[])
 {
     printf("%s\n", "Hello");
