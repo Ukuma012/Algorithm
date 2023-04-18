b10e94e62c8c0ca876e1a6baf5f4a562938d0ce7
Ukuma012 <Ukuma012@gmail.com>
2023-04-18 13:43:42 +0900

    canDistribute

--- 2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store/main.c
+++ 2064_Minimized_Maximum_of_Products_Distributed_to_Any_Store/main.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define quantitiesSize 2
 
 int n = 6;
 int quantities[quantitiesSize] = {11, 6};
 
+bool canDistribute(int k)
+{
+    int max = n * k;
+    int products = 0;
+    for (int i = 0; i < quantitiesSize; i++)
+    {
+        products += quantities[i];
+    }
+
+    if (max < products)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello");
+    printf("%d\n", canDistribute(3));
     exit(0);
 }
