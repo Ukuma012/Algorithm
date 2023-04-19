f8616bc11b5634e9087fe4e0c6aaec94944dbdb8
Ukuma012 <ukuma012@gmail.com>
2023-04-19 09:26:20 +0900

    input

--- 2602_Minimum_Operations_to_Make_All_Arrays_Elements_Equal/main.c
+++ 2602_Minimum_Operations_to_Make_All_Arrays_Elements_Equal/main.c
@@ -3,6 +3,10 @@
 
 int main(int argc, char *argv[])
 {
-    printf("%s\n", "Hello");
+    int nums[] = {3, 1, 6, 8};
+    int queries[] = {1, 5};
+    int numsSize = sizeof(nums) / sizeof(int);
+    int queriesSize = sizeof(queries) / sizeof(int);
+
     exit(0);
 }
