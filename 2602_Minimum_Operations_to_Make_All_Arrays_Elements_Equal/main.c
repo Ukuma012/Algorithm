116f64acd85ebb956187d3f9c71f50d777223b85
Ukuma012 <ukuma012@gmail.com>
2023-04-19 17:38:48 +0900

    partition

--- 2602_Minimum_Operations_to_Make_All_Arrays_Elements_Equal/main.c
+++ 2602_Minimum_Operations_to_Make_All_Arrays_Elements_Equal/main.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int partition(int a[], int l, int r) {
+    int i, j, pivot, t;
+
+    i = l - 1;
+    j = r;
+
+    pivot = a[r];
+
+    for(;;) {
+        while(a[++i] < pivot) {
+            ;
+        }
+
+        while(i < --j && pivot < a[j]) {
+            ;
+        }
+
+        if(i >= j) {
+            break;
+        }
+
+        t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    t = a[i];
+    a[i] = a[r];
+    a[r] = t;
+    return i;
+}
+
 int main(int argc, char *argv[])
 {
     int nums[] = {3, 1, 6, 8};
