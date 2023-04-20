21eb67f4d22b5891c7e81f1df0447fd83e125473
Ukuma012 <Ukuma012@gmail.com>
2023-04-20 15:17:34 +0900

    input

--- 1802_Maximum_Value_at_a_Given_Index_in_a_Bounded_Array/main.c
+++ 1802_Maximum_Value_at_a_Given_Index_in_a_Bounded_Array/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", "Hello");
+int main(int argc, char *argv[])
+{
+    int n = 4;
+    int index = 2;
+    int maxSum = 6;
+
     exit(0);
 }
